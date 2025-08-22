cea0682451d22cfe01964154e48b248233f26cc2
markus <modj62361@edu.ucl.dk>
2025-08-22 20:48:28 +0200

    backend added

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import connectDB from "./lib/connectDB.js";
 
 const app = express();
 
-console.log("Changed");
+/* app.get("/test", (req, res) => {
+  res.status(200).send("it works!");
+}); */
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
 app.listen(3000, () => {
+  connectDB();
   console.log("Server is running!");
 });
---
+++ Backend/routes/post.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getPosts, getPost } from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+
+export default router;
